eab5323c591ba24b0a0bd3fb0a1f6312b8d95e57
Lenovo <manasesmutembei11@gmail.com>
2024-03-14 14:03:20 +0300

    Initial Commit

--- Market/Program.cs
+++ Market/Program.cs
@@ -1,6 +1,21 @@
+using Market.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
+
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
 
 // Add services to the container.
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,12 +26,25 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
 
 app.UseAuthorization();
 
